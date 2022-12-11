bc29af7e2bd9ab21ccfdac03c5b7ba40c7492edf
Rikstone <tarikbouari@gmail.com>
2022-12-11 23:55:50 +0000

    Generate order data model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+    belongs_to :customer, class_name: 'Customer'
+    belongs_to :food_item, class_name: 'FoodItem'
+    has_many :reservations, class_name: 'Reservation'
 end
